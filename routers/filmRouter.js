4ae859bbba7ddc0a6076a687e2cbb972d9d0a8e7
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-06-03 12:04:58 +0200

    [UPD]Router

--- routers/filmRouter.js
+++ routers/filmRouter.js
@@ -5,13 +5,19 @@ const express = require('express');
 const router = express.Router();
 
 // Importa il controller che gestisce le operazioni sui film
-const movieController = require('../controllers/movieControllers');
+const movieController = require('../controllers/movieController'); // Assicurati che il nome del file sia corretto
 
 // Definisce una rotta GET per la homepage dei film, che richiama la funzione `index` nel controller
 router.get('/', movieController.index);
 
-// Definisce una rotta GET per ottenere un singolo film tramite il suo ID, con la funzione `show` nel controller
-router.get('/:id', movieController.show);
+// Definisce una rotta GET per ottenere un singolo film tramite il suo ID, con gestione degli errori
+router.get('/:id', async (req, res, next) => {
+    try {
+        await movieController.show(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
 // Esporta il router per poterlo usare in altre parti dell'app
 module.exports = router;
